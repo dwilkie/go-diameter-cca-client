6bd8321925aa8032ace2286941e8385a058d570d
David Wilkie <dwilkie@gmail.com>
2014-04-24 12:09:37 +0700

    initial commit

---
+++ client.go
@@ -0,0 +1,114 @@
+// Copyright 2013-2014 go-diameter authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Diameter client.
+
+package main
+
+import (
+  "flag"
+  "fmt"
+  "log"
+  "math/rand"
+  "net"
+  "os"
+  "time"
+
+  "github.com/fiorix/go-diameter/diam"
+  "github.com/fiorix/go-diameter/diam/datatypes"
+)
+
+const (
+  Identity    = datatypes.DiameterIdentity("client")
+  Realm       = datatypes.DiameterIdentity("localhost")
+  VendorId    = datatypes.Unsigned32(13)
+  ProductName = datatypes.UTF8String("go-diameter")
+  AuthApplicationId = datatypes.Unsigned32(4)
+  ServiceContextId = datatypes.UTF8String("chibi@chibitxt.me")
+  CCRequestType = datatypes.Enumerated(0x01)
+  CCRequestNumber = datatypes.Unsigned32(0)
+  RequestedAction = datatypes.Enumerated(0x00)
+  SubscriptionIdType = datatypes.Enumerated(0x00) // E164
+  SubscriptionIdData = datatypes.UTF8String("85560234567")
+)
+
+func main() {
+  ssl := flag.Bool("ssl", false, "connect using SSL/TLS")
+  flag.Parse()
+  if len(os.Args) < 2 {
+    fmt.Println("Use: client [-ssl] host:port")
+    return
+  }
+  // ALL incoming messages are handled here.
+  diam.HandleFunc("ALL", func(c diam.Conn, m *diam.Message) {
+    log.Printf("Receiving message from %s", c.RemoteAddr().String())
+    log.Println(m)
+  })
+  // Connect using the default handler and base.Dict.
+  addr := os.Args[len(os.Args)-1]
+  log.Println("Connecting to", addr)
+  var (
+    c   diam.Conn
+    err error
+  )
+  if *ssl {
+    c, err = diam.DialTLS(addr, "", "", nil, nil)
+  } else {
+    c, err = diam.Dial(addr, nil, nil)
+  }
+  if err != nil {
+    log.Fatal(err)
+  }
+  go NewClient(c)
+  // Wait until the server kick us out.
+  <-c.(diam.CloseNotifier).CloseNotify()
+  log.Println("Server disconnected.")
+}
+
+// NewClient sends a CER to the server and then a DWR every 10 seconds.
+func NewClient(c diam.Conn) {
+  // Build CCR
+  m := diam.NewRequest(272, 0, nil)
+  // Add AVPs
+  m.NewAVP("Origin-Host", 0x40, 0x00, Identity)
+  m.NewAVP("Origin-Realm", 0x40, 0x00, Realm)
+  laddr := c.LocalAddr()
+  ip, _, _ := net.SplitHostPort(laddr.String())
+  m.NewAVP("Host-IP-Address", 0x40, 0x0, datatypes.Address(net.ParseIP(ip)))
+  m.NewAVP("Vendor-Id", 0x40, 0x0, VendorId)
+  m.NewAVP("Product-Name", 0x40, 0x0, ProductName)
+  m.NewAVP("Auth-Application-Id", 0x40, 0x0, AuthApplicationId)
+  m.NewAVP("Service-Context-Id", 0x40, 0x0, ServiceContextId) // Could be optional
+  m.NewAVP("CC-Request-Type", 0x40, 0x0, CCRequestType)
+  m.NewAVP("CC-Request-Number", 0x40, 0x0, CCRequestNumber)
+  m.NewAVP("Requested-Action", 0x40, 0x0, RequestedAction)
+  m.NewAVP("Subscription-Id", 0x40, 0x00, &diam.Grouped{
+    AVP: []*diam.AVP{
+      // Subscription-Id-Type
+      diam.NewAVP(450, 0x40, 0x0, SubscriptionIdType),
+      // Subscription-Id-Data
+      diam.NewAVP(444, 0x40, 0x0, SubscriptionIdData),
+    },
+  })
+
+  log.Printf("Sending message to %s", c.RemoteAddr().String())
+  log.Println(m)
+  // Send message to the connection
+  if _, err := m.WriteTo(c); err != nil {
+    log.Fatal("Write failed:", err)
+  }
+  // Send watchdog messages every 5 seconds
+  for {
+    time.Sleep(5 * time.Second)
+    m = diam.NewRequest(280, 0, nil)
+    m.NewAVP("Origin-Host", 0x40, 0x00, Identity)
+    m.NewAVP("Origin-Realm", 0x40, 0x00, Realm)
+    m.NewAVP("Origin-State-Id", 0x40, 0x00, datatypes.Unsigned32(rand.Uint32()))
+    log.Printf("Sending message to %s", c.RemoteAddr().String())
+    log.Println(m)
+    if _, err := m.WriteTo(c); err != nil {
+      log.Fatal("Write failed:", err)
+    }
+  }
+}
